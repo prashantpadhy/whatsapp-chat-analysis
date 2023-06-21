33fbbcde38eebddd9ef77211103f019240233f45
Prashant Padhy <prashantpadhy21@gmail.com>
2023-06-21 16:32:36 +0530

    done

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from urlextract import URLExtract
-from wordcloud import WordCloud
+# from wordcloud import WordCloud
 import pandas as pd
 from collections import Counter
 import emoji
@@ -35,28 +35,28 @@ def most_busy_users(df):
         columns={'index': 'name', 'user': 'percent'})
     return x,df
 
-def create_wordcloud(selected_user,df):
+# def create_wordcloud(selected_user,df):
 
-    f = open('stop_hinglish.txt', 'r')
-    stop_words = f.read()
+#     f = open('stop_hinglish.txt', 'r')
+#     stop_words = f.read()
 
-    if selected_user != 'Overall':
-        df = df[df['user'] == selected_user]
+#     if selected_user != 'Overall':
+#         df = df[df['user'] == selected_user]
 
-    temp = df[df['user'] != 'group_notification']
-    temp = temp[temp['message'] != '<Media omitted>\n']
+#     temp = df[df['user'] != 'group_notification']
+#     temp = temp[temp['message'] != '<Media omitted>\n']
 
-    def remove_stop_words(message):
-        y = []
-        for word in message.lower().split():
-            if word not in stop_words:
-                y.append(word)
-        return " ".join(y)
+#     def remove_stop_words(message):
+#         y = []
+#         for word in message.lower().split():
+#             if word not in stop_words:
+#                 y.append(word)
+#         return " ".join(y)
 
-    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
-    temp['message'] = temp['message'].apply(remove_stop_words)
-    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
-    return df_wc
+#     wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+#     temp['message'] = temp['message'].apply(remove_stop_words)
+#     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+#     return df_wc
 
 def most_common_words(selected_user,df):
 
